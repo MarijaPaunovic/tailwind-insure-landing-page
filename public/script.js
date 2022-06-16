9702016c242ac39ee79eb0fb98510d4b0f734bc7
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-06-16 19:26:39 +0200

    finished navigation menu for mobile

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
-// Navigation toggle (hamburger btn) to add
+const btnMenuOpen = document.querySelector('[data-btn-open]');
+const btnMenuClose = document.querySelector('[data-btn-close]');
+const menu = document.querySelector('[data-menu]');
 
-console.log('hello');
+btnMenuOpen.addEventListener('click', e => {
+    e.preventDefault();
+
+    btnMenuOpen.classList.add('hidden');
+    btnMenuClose.classList.remove('hidden');
+    menu.classList.remove('hidden');
+    menu.classList.add('show');
+
+});
+
+btnMenuClose.addEventListener('click', e => {
+    e.preventDefault();
+
+    btnMenuOpen.classList.remove('hidden');
+    btnMenuClose.classList.add('hidden');
+    menu.classList.add('hidden');
+    menu.classList.remove('show');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,32 @@ module.exports = {
     './public/script.js'
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem'
+    },
     extend: {
+      screens: {
+        xs: '375px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        '2xl': '1536px',
+      },
       fontFamily: {
         title: ['DM Serif Display', 'serif'],
         body: ['Karla', 'sans-serif']
+      },
+      colors: {
+        darkViolet: 'hsl(256, 26% 20%)',
+        grayishBlue: 'hsl(216, 30%, 68%)',
+        veryDarkViolet: 'hsl(270, 9%, 17%)',
+        darkGrayishViolet: 'hsl(273, 4%, 51%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
+      },
+      backgroundImage: {
+        'menu-pattern': "url('./images/bg-pattern-mobile-nav.svg')",
       }
     },
   },
